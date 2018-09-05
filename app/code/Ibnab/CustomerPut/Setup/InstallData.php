c44259a2d32a12cbb0b8bec0de875a1f51d7d979
Ravi <raveendrasunkavalli@gmail.com>
2018-09-05 11:20:55 +0530

    First commit

---
+++ app/code/Ibnab/CustomerPut/Setup/InstallData.php
@@ -0,0 +1,71 @@
+<?php
+namespace Ibnab\CustomerPut\Setup;
+
+use Magento\Framework\Module\Setup\Migration;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Customer setup factory
+     *
+     * @var \Magento\Customer\Setup\CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+    /**
+     * Init
+     *
+     * @param \Magento\Customer\Setup\CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(\Magento\Customer\Setup\CustomerSetupFactory $customerSetupFactory)
+    {
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+    /**
+     * Installs DB schema for a module
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+
+        $installer = $setup;
+        $installer->startSetup();
+
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, 'phone', [
+            'type' => 'varchar',
+            'label' => 'phone',
+            'input' => 'text',
+            'source' => '',
+            'required' => true,
+            'visible' => true,
+            'position' => 333,
+            'system' => false,
+            'backend' => ''
+        ]);
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'phone')
+            ->addData(['used_in_forms' => [
+                'adminhtml_customer',
+                'adminhtml_checkout',
+                'customer_account_create',
+                'customer_account_edit'
+            ]
+            ]);
+        $attribute->setData("is_used_for_customer_segment", true)
+            ->setData("is_system", 0)
+            ->setData("is_user_defined", 1)
+            ->setData("is_visible", 1)
+            ->setData("sort_order", 100);
+        $attribute->save();
+
+        
+        $installer->endSetup();
+    }
+}
